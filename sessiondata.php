be27ee8fc609d1c0cea6860fa859c85dbbbb5911
rockstar877698-dotcom <rockstar877698@gmail.com>
2025-08-28 02:33:30 -0700

    Update sessiondata.php

--- sessiondata.php
+++ sessiondata.php
@@ -1,8 +1,11 @@
 <?php
  session_start();
 test
+
+test 3
  echo 'The Name of the student is :' . $_SESSION["Name"] . '<br>';
  echo 'The Roll number of the student is :' . $_SESSION["Rollnumber"] . 
 '<br>';
 
  ?>
+
