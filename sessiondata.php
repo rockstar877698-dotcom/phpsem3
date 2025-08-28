5914fabaa36d44a27d2a17e847c8bf65336c0882
rockstar877698-dotcom <rockstar877698@gmail.com>
2025-08-28 02:42:20 -0700

    Update sessiondata.php
    remove 2 line

--- sessiondata.php
+++ sessiondata.php
@@ -1,11 +1,9 @@
 <?php
  session_start();
-test
-
-test 3
  echo 'The Name of the student is :' . $_SESSION["Name"] . '<br>';
  echo 'The Roll number of the student is :' . $_SESSION["Rollnumber"] . 
 '<br>';
 
  ?>
+
 
